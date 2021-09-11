7bab47f19608d47bb8f7fd22f1f1d54cc8f0bf16
admin <root@localhost>
2021-09-11 16:16:10 -0700

    Add which-key plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -337,4 +337,16 @@ return packer.startup(function()
          require("core.mappings").vim_fugitive()
       end,
    }
+
+   use {
+      "folke/which-key.nvim",
+      config = function()
+        require("which-key").setup {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        } 
+      end,
+   }
+
 end)
